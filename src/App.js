4212ddf12baba24a70500c3d9836a541afff9b4a
philiptk078 <philiptk078@gmail.com>
2021-06-03 11:27:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import NavBar from './Components/NavBar/NavBar';
+import RowPost from './Components/RowPost/RowPost';
+import {originals,action,romance} from './urls'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={romance} title='Romance Movies' isSmall/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '5cca2f7b2fa4846fa0cfa3762718d974';
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
